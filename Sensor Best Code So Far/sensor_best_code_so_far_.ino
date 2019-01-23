deb6bfc9ca2f745824848a78bec040a9893187c3
Nestor30 <Nestorceballos75@gmail.com>
2019-01-23 15:46:17 -0500

    Add files via upload

---
+++ Sensor Best Code So Far/sensor_best_code_so_far_.ino
@@ -0,0 +1,27 @@
+#include <NewPing.h>
+#include <Servo.h>
+
+#define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup() {
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+
+}
+
+void loop() {
+  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
+
+  if(sonar.ping_cm() < 20){
+    Serial.print("Detection systems detected something at: ");
+  }else{
+    Serial.print("Nothing on the way, current distance: ");
+  }
+}
+
